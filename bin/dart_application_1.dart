b4eebe5574845f14fac12f8796ad182d584062b3
ruslan301287 <koreec.959570@gmail.com>
2023-12-11 19:33:09 +0600

    first commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,23 @@
+//task1
+void main() {
+  double b = 4.20;
+  int a = 3;
+
+  print(b + a);
+}
+
+//task2
+void main() {
+  String text = 'Name';
+  text = 'Flutter';
+
+  print('I want to learn $text');
+}
+
+//task3
+void main() {
+  double myNumber = 2.225;
+  String myText;
+  myText = myNumber.toString();
+  print(myText.runtimeType);
+}
